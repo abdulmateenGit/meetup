9271f979f2d5337654552ebfac3c00a943b7d2a9
abdulmateenGit <abdulmateen.lhr00@gmail.com>
2024-09-27 22:45:26 +0500

    Parse and display date using dayjs

--- components/EventListItem.tsx
+++ components/EventListItem.tsx
@@ -1,15 +1,22 @@
 import { Feather } from '@expo/vector-icons';
 import { Image, Text, View } from 'react-native';
+import dayjs from 'dayjs';
 
 
 export default function EventListItem({ event }) {
     return (
-        <View className='p-3 gap-3'>
+        <View className='p-3 gap-3 border-b-2 border-gray-100 pb-3'>
             <View className='flex-row'>
                 <View className='flex-1 gap-2'>
-                    <Text className='text-lg font-semibold uppercase text-amber-800'> Wed 13, Sep · 19:30 CET </Text>
-                    <Text className='text-xl font-bold' numberOfLines={2}> {event.title}</Text>
-                    <Text className='text-gray-700'> {event.location} </Text>
+                    <Text className='text-lg font-semibold uppercase text-amber-800'>
+                        {dayjs(event.datetime).format('ddd, D MMM')} · {dayjs(event.datetime).format('hh:mm A')}
+                    </Text>
+                    <Text className='text-xl font-bold' numberOfLines={2}>
+                        {event.title}
+                    </Text>
+                    <Text className='text-gray-700'>
+                        {event.location}
+                    </Text>
                 </View>
                 <Image
                     source={{ uri: event.image }}

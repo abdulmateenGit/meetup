c5abcfd9d352ad99ab8d4679f1da9d85c87ae4cf
abdulmateenGit <abdulmateen.lhr00@gmail.com>
2024-10-06 08:54:47 +0500

    Events Table and Fatching

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,27 +1,45 @@
-import { View, Text, Image, Pressable } from 'react-native'
-import React from 'react'
+import { View, Text, Image, Pressable, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { useLocalSearchParams, Stack } from 'expo-router'
-import events from "~/assets/events.json"
 import dayjs from 'dayjs'
+import { supabase } from '~/utils/supabase'
 
 export default function EventPage() {
   const { id } = useLocalSearchParams()
-  const event = events.find(e => e.id === id);
+  const [event, setEvent] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    fetchEvent()
+  }, [id])
+
+
+  const fetchEvent = async () => {
+    setLoading(true)
+    const { data, error } = await supabase.from('events').select('*').eq('id', id).single();
+    setEvent(data)
+    setLoading(false)
+  }
+
+  if (loading) {
+    return <ActivityIndicator />
+  }
+
   if (!event) {
     return <Text>Event not found</Text>
   }
   return (
     <View className='flex-1 gap-3 bg-white p-3'>
-      <Stack.Screen options={{ title: "Event", headerBackTitleVisible: false }} />
+      <Stack.Screen options={{ title: "Event", headerBackTitleVisible: false, headerTintColor: 'black' }} />
       <Image
-        source={{ uri: event.image }}
+        source={{ uri: event.image_uri }}
         className="aspect-video w-full rounded-xl"
       />
       <Text className='text-3xl font-bold' numberOfLines={2}>
         {event.title}
       </Text>
       <Text className='text-lg font-semibold uppercase text-amber-800'>
-        {dayjs(event.datetime).format('ddd, D MMM')} · {dayjs(event.datetime).format('hh:mm A ')}
+        {dayjs(event.date).format('ddd, D MMM')} · {dayjs(event.date).format('hh:mm A')}
       </Text>
       <Text className='text-lg' numberOfLines={2}>
         {event.description}
